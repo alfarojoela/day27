8934293d617d0bd68d27ab344fce244a49e693ac
Joel Alfaro <alfarojoela@outlook.com>
2021-10-06 11:52:28 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import tkinter
+
+window = tkinter.Tk()
+
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+#Label
+
+my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#line of code keeps window on screen.
+window.mainloop()
